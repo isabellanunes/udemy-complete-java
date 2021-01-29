d4cddd26ca15c2300fe69f5ac9dd86d245e52789
Isabella de Freitas Nunes <isabella.nunes@philips.com>
2021-01-29 19:32:46 -0300

    Added the third code to exercises03

---
+++ exercises03/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+?
+Exercise01.java,0\a\0a458897a54f74f767a80ab380b78e998287efac
+?
+Exercise02.java,1\b\1bce763f021c7207a7b43b89f8c6e13b0d870252
+?
+Exercise03.java,a\7\a7eaf71c97e82f8adc69d1d14ca53ac07f18dc53
---
+++ exercises03/Exercise03.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Exercise03 {
+
+    public static void main(String[] args) {
+
+        int alcohol = 0, gasoline = 0, diesel = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        int input = scanner.nextInt();
+
+        while(input != 4) {
+
+            if(input == 1) {
+                alcohol += 1;
+            } else if(input == 2) {
+                gasoline += 1;
+            } else if(input == 3) {
+                diesel += 1;
+            }
+
+            input = scanner.nextInt();
+            
+        }
+
+        System.out.println("MUITO OBRIGADO");
+        System.out.println("Alcool: " + alcohol);
+        System.out.println("Gasolina: " + gasoline);
+        System.out.println("Diesel: " + diesel);
+
+        scanner.close();
+    }
+}
